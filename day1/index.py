c2a7a46081074005cd486d0934f923c51cff4584
Huma Fahad Siddqui <167395734+HumafahadSidd@users.noreply.github.com>
2025-02-24 11:19:23 +0500

    Add files via upload

---
+++ day1/index.py
@@ -0,0 +1,6 @@
+ #Printing a welcome message and name
+print("Hello, Python Learners! 🚀 Today is the first step in our coding journey.")
+
+# Modify the program to include your name
+name = input("Enter your name: ")  # Taking user input for the name
+print("Welcome,", name, "! Let's start coding! 🚀")
